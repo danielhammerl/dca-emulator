b0305d6389dc3a994279f0826dc0b7b3858730c0
danielhammerl <daniel.hammerl99@gmx.de>
2022-11-04 16:32:46 +0100

    v0.1 - refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
+#!/usr/bin/env ts-node-script
 import fs from "fs";
 import { run } from "./emulator";
-import {EOL} from 'os';
+import { EOL } from "os";
 
-const sourceCode = fs.readFileSync("./source.dbin", { encoding: "utf-8" }).replaceAll(EOL, " ");
-console.log(sourceCode);
+const sourceCode = fs
+  .readFileSync(process.argv[2] || "./source.dbin", { encoding: "utf-8" })
+  .replaceAll(EOL, " ");
+
+if (process.argv[3] === "--debug") {
+  process.env.DEBUG = "true";
+}
 
 run(sourceCode, process.env.DEBUG === "true");
